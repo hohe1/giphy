9becc256766320b4df10ec46aa851b7bc5f3acb6
Hongjun He <anthonyho02015@gmail.com>
2019-02-27 02:13:53 +0000

    completed day 1 task

--- js/script.js
+++ js/script.js
@@ -2,10 +2,44 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var site_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+var pic_url = "var pic_url = response.data.images.original.url";
 
 $("#search-button").click(function(){
   
-  
-  
-});
+$.ajax({
+    url:site_url,
+    method: "GET",
+    success(response){
+        console.log(site_url);
+
+        for(let i= 0;i<response.data.length;i++){
+            let image = response.data[i].images.original.url;
+            $(".gallery").append(`<img src=${image}>`);
+            }
+        }
+    });
+    
+    //$("#search-term").val()
+    
+    
+    
+    
+    
+    
+    
+    
+});    
+    
+
+
+
+// $.ajax({
+//     url:pic_url,
+//     method: "GET",
+//     success(response){
+//         console.log(response.data[0].url)
+//     }
+    
+// });
 
